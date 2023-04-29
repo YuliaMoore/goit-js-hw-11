62a46e5ba0354b20c34f0070546633d639386631
YuliaMoore <115689321+YuliaMoore@users.noreply.github.com>
2023-04-29 08:38:14 +0300

    2

--- src/api/fetchImages.js
+++ src/api/fetchImages.js
@@ -2,24 +2,23 @@ import axios from 'axios';
 const KEY = '35800613-7b32faaa33505b6c7b58566c2';
 const BASE_URL = 'https://pixabay.com/api/';
 // https://pixabay.com/api/?key=35800613-7b32faaa33505b6c7b58566c2&q
-fetch(`${BASE_URL}?key=${KEY}&q=lion`);
 
-// function createGallery
-// return `
-// <div class="photo-card">
-//   <img src="" alt="" loading="lazy" />
-//   <div class="info">
-//     <p class="info-item">
-//       <b>Likes</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Views</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Comments</b>
-//     </p>
-//     <p class="info-item">
-//       <b>Downloads</b>
-//     </p>
-//   </div>
-// </div>`;
+export async function fetchUrl(searchQuery, pageCount) {
+  const params = {
+    params: {
+      key: KEY,
+      q: searchQuery,
+      image_type: 'photo',
+      orientation: 'horizontal',
+      safesearch: 'true',
+      page: pageCount,
+      per_page: 40,
+    },
+  };
+  try {
+    const response = await axios.get(BASE_URL, params);
+    return response.data;
+  } catch (error) {
+    console.log(error.message);
+  }
+}
--- src/index.js
+++ src/index.js
@@ -1,11 +1,92 @@
+import { Notify } from 'notiflix';
+
 // import { fetchUrl } from './js/api/fetchImages';
 const KEY = '35800613-7b32faaa33505b6c7b58566c2';
 const BASE_URL = 'https://pixabay.com/api/';
 // https://pixabay.com/api/?key=35800613-7b32faaa33505b6c7b58566c2&q
-fetch(`${BASE_URL}?key=${KEY}&q=lion`)
-  .then(data => data.json())
-  .then(data => console.log(data));
-
+function getImages() {
+  fetch(`${BASE_URL}?key=${KEY}&q=lion`)
+    .then(data => data.json())
+    .then(data => console.log(data));
+}
 const form = document.querySelector('#search-form');
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
+
+form.addEventListener('submit', onSubmit);
+loadMore.addEventListener('click', onClick);
+
+let pageNumber = 1;
+loadMore.getElementsByClassName.display = 'none';
+
+const gallerySimpleBox = new SimpleLightbox('.gallery a');
+
+async function onSubmit(ev) {
+  ev.preventDefault();
+  refreshGallery();
+  const searchValue = form.elements.searchQuery.value;
+
+  const response = await fetchUrl(searchValue, pageNumber);
+  if (response.hits.lenght === 0) {
+    Notify.failure(
+      'Sorry, there are no images matching your search query. Please try again.'
+    );
+  } else {
+    renderImages(response.hits);
+    gallerySimpleBox.refresh();
+    Notify.success(`Hooray! We found ${response.totalHits} images.`);
+
+    if (response.totalHits === response.hits.lenght) {
+      loadMore.style.display = 'none';
+      Notify.failure(
+        "We're sorry, but you've reached the end of search results."
+      );
+    } else {
+      loadMore.style.display = 'block';
+    }
+  }
+  console.log(response);
+}
+
+function renderImages(images) {
+  const markUp = images.map(image => {
+    return `
+<div class="photo-card">
+ <a href="${image.largeImageURL}"> <img class="photo-card__img" src="${image.webformatURL}" alt="${image.tags}" loading="lazy" width="320" height="212" /></a>
+  <div class="info">
+    <p class="info-item">
+      <b>Likes: ${image.likes}</b>
+    </p>
+    <p class="info-item">
+      <b>Views: ${image.views}</b>
+    </p>
+    <p class="info-item">
+      <b>Comments: ${image.comments}</b>
+    </p>
+    <p class="info-item">
+      <b>Downloads: ${image.downloads}</b>
+    </p>
+  </div>
+</div>`;
+  });
+  gallery.innerHTML += markUp;
+}
+
+async function onClick() {
+  const searchValue = form.elements.searchQuery.value;
+  pageNumber += 1;
+  const response = await fetchUrl(searchValue, pageNumber);
+  renderImages(response.hits);
+  if (response.totalHits === response.hits.length) {
+    loadMore.style.display = 'none';
+    Notify.failure(
+      "We're sorry, but you've reached the end of search results."
+    );
+  }
+}
+
+function refreshGallery() {
+  gallery.innerHTML = '';
+  pageNumber = 1;
+  loadMore.style.display = 'none';
+}
